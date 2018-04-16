c6366ce6ca5b1a66eb48b92047dbc44ab22595dd
DeviKrishna <DeviKrishna_gs@infosys.com>
2018-04-16 15:57:49 +0530

    devi

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "hi Hello World");
+		get("/hello", (req, res) -> "hi hi Hello World");
 	}
 }
